e857e59002bd3029b6ae4d3e1f423e68d1ad58a4
Daniil Litvinov <48359219+danon6868@users.noreply.github.com>
2020-12-10 12:18:04 +0300

    Update merger.sh

--- merger.sh
+++ merger.sh
@@ -4,15 +4,11 @@ i=87
 while [ $i -gt 68 ]
 files=()
 do
-for var in $(ls | grep -E "^III_CH027${i}_S[[:alnum:]]*_[[:alnum:]]*_R2")
+for var in $(ls | grep -E "^[condition_number]_CH027${i}_S[[:alnum:]]*_[[:alnum:]]*_R1/2")
 do
 files+=( $var )
 done
-zcat ${files[0]} ${files[1]} | gzip -c > ~/../../rnaseq/RNAskyrunners/alignment_ready/III_CH027${i}_R2_merged.fastq.gz
+zcat ${files[0]} ${files[1]} | gzip -c > ~/../../rnaseq/RNAskyrunners/alignment_ready/[condition_number]_CH027${i}_R1/2_merged.fastq.gz
 i=$[ $i - 1 ]
-#if [ $i -eq 68 ]
-#then
-#break
-#fi
 done
 
